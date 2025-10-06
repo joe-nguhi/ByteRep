bdfdbdc57609de608a4033fb2c87a948f3cbc782
Nguhi <nguhi.dev@gmail.com>
2025-10-06 22:30:02 +0300

    feat(FreeCodeCamp): add Space Week Day 3 - Phone Home problem and solution

---
+++ FreeCodeCamp/SpaceWeekDay3 - Phone Home/solution.py
@@ -0,0 +1,15 @@
+SPEED_OF_LIGHT = 300_000
+SAT_DELAY = 0.5
+def send_message(route):
+    total_distance = 0
+    for dst in route:
+        total_distance += dst
+
+    n_sats = len(route) - 1
+    
+    return round(total_distance/SPEED_OF_LIGHT + n_sats*SAT_DELAY, 4)
+
+
+print(send_message([300_000, 300_000]))
+print(send_message([384_400, 384_400]))
+print(send_message([1_000_000,500_000_000,1_000_000]))
