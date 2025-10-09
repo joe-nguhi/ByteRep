cb865ad8ef083282719b70dc890ec1b445df1255
Nguhi <nguhi.dev@gmail.com>
2025-10-09 13:35:16 +0300

    feat(FreeCodeCamp): add solution for Stellar Classification
    - Implement surface temperature classification for stars
    - Create main function to demonstrate classification results

---
+++ FreeCodeCamp/Stellar Classification/solution.go
@@ -0,0 +1,34 @@
+package main
+
+import "fmt"
+
+/* SpaceWeek Day 1:Stellar Classification */
+func main() {
+
+	fmt.Printf("Temp:\t5778  Star Class: %s\n", classification(5778))
+	fmt.Printf("Temp:\t2400  Star Class: %s\n", classification(2400))
+	fmt.Printf("Temp:\t9999  Star Class: %s\n", classification(9999))
+
+}
+
+// classification Given the surface temperature of a star in Kelvin(K),return its stellar classication
+func classification(temperature int) string {
+	switch {
+	case temperature >= 30000:
+		return ("O")
+	case temperature >= 10000:
+		return "B"
+	case temperature >= 7500:
+		return "A"
+	case temperature >= 6000:
+		return "F"
+	case temperature >= 5200:
+		return "G"
+	case temperature >= 3700:
+		return "K"
+	case temperature >= 0:
+		return "M"
+	default:
+		return "Unclassified"
+	}
+}
